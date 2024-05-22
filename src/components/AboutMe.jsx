55572932f9192431caf29d73a8e332e6d3a61277
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-05-22 18:37:00 +0200

    Image added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,12 +1,14 @@
+import FenPhoto from "../media/fen.jpg";
+
 export default function AboutMe() {
     return (
         <section>
-            <div class="container px-4 py-5">
-                <h2 class="pb-2 ">ABOUT ME</h2>
-                <div class="row pt-5 mt-5">
-                    <div class="col-sm-12 col-lg-6">
-                        <img src="" alt="photo of Fenila" />
-                        <ul class="pt-2 list-unstyled ">
+            <div className="container px-4 py-5">
+                <h2 className="pb-2 ">ABOUT ME</h2>
+                <div className="row pt-5 mt-5">
+                    <div className="col-sm-12 col-lg-6">
+                        <img src={FenPhoto} alt="photo of Fenila" width={250} height={300} />
+                        <ul className="pt-2 list-unstyled ">
                             <li>
                                 <h4>Fenila Sosa Philip</h4>
                             </li>
