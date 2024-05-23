a22ec8e4ff0215347c4c4bbed4e29d2f25d86b87
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-05-23 13:50:38 +0200

    my picture added to home page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,14 +1,21 @@
 import girlImg from "../media/girl.jpg";
+import FenPhoto from "../media/fen.jpg";
 
 export default function HomePage() {
     return (
         <section id="home">
             <div className="container">
                 <div className="row p-2 mt-5 ">
-                    <div className="col-sm-12 col-lg-6">
+                    <div id="introImg" className="col-sm-12 col-lg-6">
                         <img src={girlImg} alt="photo of girl" width="500" height="700" />
+                        <div id="fenPhoto">
+                            <img src={FenPhoto} alt="photo of Fenila" width="250" height="300" />
+                        </div>
                     </div>
                     <div id="intro" className="pt-5 col-sm-12 col-lg-6">
+                        <div id="myPhoto">
+                            <img src={FenPhoto} alt="photo of Fenila" width="250" height="300" />
+                        </div>
                         <h1 className="mt-5">FENILA SOSA PHILIP</h1>
                         <h2>FRONTEND DEVELOPER</h2>
                         <p>
