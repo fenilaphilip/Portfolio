09b1331006d0505ad09ed7726e59fbe3346e41b8
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-05-24 10:02:26 +0200

    Adding more projects content

---
+++ src/assets/myworks.js
@@ -0,0 +1,41 @@
+import weather from "../media/weather.png";
+import dictionary from "../media/dictionary.png";
+import investmentCalculator from "../media/investmentCalculator.png";
+import boutique from "../media/boutique.png";
+
+const MY_WORKS = [
+    {
+        id: 1,
+        caption: "WEATHER APP",
+        img: weather,
+        img_alt: "app view",
+        description: "A React Web App that utilizes the OpenWeatherMap API to return current and 7- day forecasted weather for a given location.",
+        link: "https://weather.fenilas.com/",
+    },
+    {
+        id: 2,
+        caption: "DICTIONARY APP",
+        img: dictionary,
+        img_alt: "app view",
+        description: "The React Dictionary App is a web application that enables users to search for word definitions, synonyms, antonyms, and example sentences. It offers a user-friendly interface for vocabulary exploration and enrichment.",
+        link: "https://aquamarine-cendol-64be35.netlify.app/",
+    },
+    {
+        id: 3,
+        caption: "INVESTMENT CALCULATOR",
+        img: investmentCalculator,
+        img_alt: "app view",
+        description: "This app designed to easily assess the value of your investments over a specified period. With a user-friendly and straightforward interface, the app displays an investment value table for each year. ",
+        link: "https://classy-sundae-705307.netlify.app/",
+    },
+    {
+        id: 4,
+        caption: "ELEGANT BOUTIQUE",
+        img: boutique,
+        img_alt: "app view",
+        description: "A demo e-commerce store built with vanilla JavaScript, React, Redux and Tailwind CSS. You can select multiple numbers of clothes. Adding them to cart, removing them from cart, decreasing and increasing the cart amount and calculating the total cost of the cart ",
+        link: "https://master--dazzling-dodol-f7d555.netlify.app/",
+    },
+]
+
+export default MY_WORKS;
