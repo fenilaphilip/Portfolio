6555fe37e2809c4185c12350e40641cf5ed90a6c
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-05-25 13:00:32 +0200

    Bug fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,27 +6,31 @@ export default function Navbar() {
 
     return (
         <nav id="menu" className="navbar navbar-expand-md navbar-dark fixed-top" >
-            <div className="container">
+            <div className="container-fluid">
                 <a className="navbar-brand mx-2 me-3" href="#home">
                     f_<FaCode />_p
                 </a>
                 <button className="navbar-toggler"
                     type="button"
                     data-bs-toggle="collapse"
-                    data-bs-target="#burger-nav-menu"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
                     aria-expanded="false"
                     aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
-                <div className="collapse navbar-collapse" id="burger-nav-menu">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto  gap-3 mb-2 mb-md-0">
                         {
                             NAVBAR_ITEM.map((item) => {
                                 return (
                                     <li className="topnav nav-item" key={item.id}>
                                         <a href={item.link} className="nav-link  px-2">
-                                            {item.title}
+                                            <span data-bs-toggle="collapse"
+                                                data-bs-target=".navbar-collapse.show"
+                                            >
+                                                {item.title}
+                                            </span>
                                         </a>
                                     </li>
                                 );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,7 +5,7 @@ export default function Projects() {
         <section id="projects">
             <div className="container px-4 py-5">
                 <h2 className="mt-5 pb-2 text-center">PROJECTS</h2>
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 g-3 py-5">
+                <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 g-3 py-5">
                     <Website />
                 </div>
                 {/* <div className="d-flex  justify-content-end me-5">
