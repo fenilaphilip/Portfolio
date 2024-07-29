b49481eeab9018d03cd0a697ddfada48bef25c8c
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2024-07-29 19:49:18 +0200

    Improved user interface of projects section.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,16 @@
 import Website from "./Website";
 
+import MY_WORKS from "../assets/myworks"
+
 export default function Projects() {
     return (
         <section id="projects">
             <div className="container px-4 py-5">
                 <h2 className="mt-5 pb-2 text-center">PROJECTS</h2>
-                <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 g-3 py-5">
-                    <Website />
+                <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3 py-5">
+                    {MY_WORKS.map((project) => {
+                        return (<Website key={project.id} project={project} />)
+                    })}
                 </div>
                 {/* <div className="d-flex  justify-content-end me-5">
                     <div className="btn-group">
--- src/components/Website.jsx
+++ src/components/Website.jsx
@@ -1,29 +1,50 @@
-import MY_WORKS from "../assets/myworks"
+import { useState } from "react"
 
-export default function Website() {
+export default function Website({ project }) {
+    const [toggleCard, setToggleCard] = useState(false);
+    function flipCard(event) {
+        event.preventDefault();
+        setToggleCard(!toggleCard);
+    }
+
     return (
-        <>
-            {
-                MY_WORKS.map((project) => {
-                    return (
-                        <div className="p-3" key={project.id}>
-                            <div className="card shadow-sm p-2">
-                                <img src={project.img} alt={project.img_alt} width="100%" />
-                                <div className="card-body ">
-                                    <h3>{project.caption}</h3>
-                                    <p className="card-text">{project.description}</p>
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <div className="btn-group">
-                                            <a href={project.link} target="_blank" className="btn btn-sm btn-outline-dark">Live View</a>
-                                        </div>
-
-                                    </div>
-                                </div>
+        <div className="p-3" key={project.id}>
+            {!toggleCard && (
+                <div className="card shadow-sm p-2">
+                    <img src={project.img} alt={project.img_alt} width="100%" />
+                    <div className="card-body">
+                        <h3>{project.caption}</h3>
+                        <p className="card-text">
+                            <span id="projectDescription">
+                                {project.description}
+                            </span><a href="" onClick={flipCard}>Read more</a>
+                        </p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="btn-group">
+                                <a href={project.link} target="_blank" className="btn btn-sm  btn-outline-dark">Live View</a>
                             </div>
                         </div>
-                    )
-                })
-            }
-        </>
+                    </div>
+                </div>
+            )}
+            {toggleCard && (
+                <div className="card shadow-sm p-2">
+                    <div className="card-body">
+                        <h3>{project.caption}</h3>
+                        <p className="card-text">
+                            {project.description}
+                        </p>
+                        <div className="d-flex justify-content-between align-items-center">
+                            <div className="btn-group">
+                                <a href={project.link} target="_blank" className="btn btn-sm  btn-outline-dark">Live View</a>
+                            </div>
+                            <div className="btn-group">
+                                <a href="" onClick={flipCard} className="btn btn-sm  btn-outline-dark">Go Back</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     )
 }
