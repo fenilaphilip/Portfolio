59a449850765411760b12ca697a5ac2ab937bab0
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-12 16:18:49 +0100

    Restructing the elements

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -5,38 +5,28 @@ import ContactForm from "./ContactForm";
 export default function HomePage() {
 
     return (
-        <section id="home">
-            <div className="container">
-                <div className="row p-2 mt-5 ">
-                    <div id="introImg" className="col-sm-12 col-lg-6">
-                        <img src={girlImg} alt="photo of girl" width="500" height="700" />
-                        <div id="fenPhoto">,
-                            <img src={PROFILE.photo} alt="photo of Fenila" width="250" height="300" />
+        <section id="home" >
+            <div className="container my-5 mt-5">
+                <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center">
+                    <div className="col-lg-7 p-3 p-lg-5 pt-lg-3">
+                        <h1 className="display-4 fw-bold lh-1">{PROFILE.name}</h1>
+                        <p className="lead">{PROFILE.job}</p>
+                        <hr />
+                        <p className="lead">
+                            {PROFILE.intro}
+                        </p>
+                        <p className="lead">{PROFILE.dream} </p>
+                        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+                            <ContactForm />
+
                         </div>
                     </div>
-                    <div id="intro" className="pt-5 col-sm-12 col-lg-6">
-                        <div id="myPhoto">
-                            <img src={PROFILE.photo} alt="photo of Fenila" width="250" height="300" />
-                        </div>
-                        <div className="m-2">
-                            <h1 className="mt-5">{PROFILE.name}</h1>
-                            <h2>{PROFILE.job}</h2>
-                            <p>
-                                {PROFILE.intro}
-                            </p>
-                            <p className="border-top">{PROFILE.dream} </p>
-                            <div className="d-flex justify-content-between align-items-center">
-                                <div className="btn-group">
-                                    <a href={PROFILE.resume} className="btn btn-sm btn-outline-light me-3" target="_blank" download>RESUME</a>
-                                    <ContactForm />
-                                </div>
-
-                            </div>
-                        </div>
-
+                    <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg pt-0">
+                        <img class="rounded-lg-3" src={girlImg} alt="" width="720" height="770" />
                     </div>
                 </div>
             </div>
-        </section>
+
+        </section >
     )
 }
