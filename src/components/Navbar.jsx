79158a94148fcb607e63bb32fc6d4aceea41eaf6
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-13 11:08:30 +0100

    animating bubbles css added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,63 @@ import { NavLink } from "react-router";
 export default function Navbar() {
 
     return (
-        <nav id="menu" className="navbar navbar-expand-md bg-#5c2056" >
-            <div className="container fixed-top ">
-                <NavLink to="/">
-                    Home
-                </NavLink>
-                <NavLink to="/about">
-                    About
-                </NavLink>
-                <NavLink to="/projects">
-                    Projects
-                </NavLink>
-                <NavLink to="/contact">
-                    Contact
-                </NavLink>
+        <>
+            <div className="bubbles">
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
+                <div className="bubble"></div>
             </div>
-        </nav >
+            <nav className="navbar navbar-expand  p-1" style={{
+                backgroundColor: "#803b7a"
+            }}>
+                <div className="container justify-content-md-left ">
+                    <ul className="navbar-nav" >
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link active" aria-current="page">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/about" className="nav-link">
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/projects" className="nav-link">
+                                Projects
+                            </NavLink>
+                        </li>
+                        <li className="nav-item ">
+                            <NavLink to="/contact" className="nav-link" >
+                                Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+
+                </div>
+            </nav >
+        </>
     );
 }
