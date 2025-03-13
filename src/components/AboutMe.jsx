00702daf47a4e73e9236c77e94fda1bde7725095
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-13 17:34:44 +0100

    Restructed elements and skills added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,43 +1,35 @@
 import { PROFILE } from "../assets/data"
+import Skills from "./Skills"
 
 export default function AboutMe() {
     return (
         <section id="about">
-            <div className="container px-4 py-5">
-                <h2 className="mt-5 pb-2 text-center text-dark">ABOUT ME</h2>
-                <div className="row pt-3">
-                    <div className="col-sm-12 col-lg-3">
-                        <img src={PROFILE.photo} alt="photo of Fenila" width={250} height={300} />
-                        <ul className="pt-2 list-unstyled text-dark ">
-                            <li>
-                                <h4>Fenila Sosa Philip</h4>
-                            </li>
-                            <li>
-                                Frontend Developer
-                            </li>
-                            <li>
-                                &
-                            </li>
-                            <li>
-                                Civil Engineer
-                            </li>
-                        </ul>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <a href={PROFILE.resume} className="btn btn-sm btn-outline-dark me-3" target="_blank" download>RESUME</a>
+            <div className="container">
+                <div className="row justify-content-center align-items-center mx-2 pt-3">
+                    <div className="col-9 col-lg-3">
+                        <div className="about_img p-4">
+                            <img src={PROFILE.photo} alt="idea-images" />
                         </div>
+                        {/* <div className="d-flex justify-content-between align-items-center">
+                            <a href={PROFILE.resume} className="btn btn-sm btn-outline-dark me-3" target="_blank" download>RESUME</a>
+                        </div> */}
                     </div>
-                    <div className="col-lg-8 text-dark">
-                        <hr />
-                        {PROFILE.about.map(para => {
-                            return (
-                                <p className="p-2">
-                                    {para}
-                                </p>
-                            )
-                        })}
+                    <div className="content col-12 col-lg-6 p-4">
+                        <div className="text-center">
+                            <h1>About</h1>
+                            <p className="mb-5">{PROFILE.about.map(para => {
+                                return (
+                                    <p className="p-2">
+                                        {para}
+                                    </p>
+                                )
+                            })}</p>
+
+                        </div>
                     </div>
                 </div>
             </div>
+            <Skills />
         </section>
     )
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,26 +4,20 @@ import SKILLS from "../assets/skills.js";
 export default function Skills() {
     return (
         <section id="skills">
-            <div className="container px-4 py-5">
-                <h2 className="mt-5 pb-2 text-center text-dark">SKILLS</h2>
+            <div className="container">
+                <h2 className="mt-5 pb-2 text-center text-dark">💻 Tech Stack I use</h2>
                 <div className="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
                     {
                         SKILLS.map((item) => {
                             return (
                                 <div className="col" key={item.title}>
-                                    <div className="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
-                                        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+                                    <div className="card card-cover h-100 overflow-hidden rounded-4 shadow-lg">
+                                        <div className="d-flex flex-column h-100 p-5 pb-3  text-shadow-1">
                                             <h3>{item.title}</h3>
                                             <ul>
                                                 {item.skills.map((skill) => {
                                                     return (
                                                         <li key={skill.name} >
-
-                                                            {/* {
-                                                                (skill.name === " HTML") ?
-                                                                    skill.symbol()
-                                                                    : ""
-                                                            } */}
                                                             {skill.name}
                                                         </li>
                                                     )
@@ -40,7 +34,10 @@ export default function Skills() {
 
                 </div>
             </div>
-
+            {/* <div className="container px-4 py-5">
+                <h1>💻 Tech Stack: </h1>
+                <img src="https://skillicons.dev/icons?i=ts,js,php,html,css,react,redux,bootstrap,materialui,tailwind,nextjs,jquery,git,npm,nodejs,express,vscode,jest,cypress,babel,github,postgres,mongodb,mysql,postman,vite,figma,sass,vim,&theme=dark" />
+            </div> */}
         </section >
 
     )
