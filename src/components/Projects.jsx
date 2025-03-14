47380cd73ffa3d4eadd90e254e85c639ef6ff5d9
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-14 11:08:52 +0100

    added new design and showing techstack

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,18 @@
 import Website from "./Website";
-
+import backgroundImg from "../media/background.jpg";
 import MY_WORKS from "../assets/myworks"
 
 export default function Projects() {
     return (
-        <section id="projects" >
+        <section id="projects" className="bg-secondary" >
             <div className="container">
-                <h2 className="text-center pt-2">My Works</h2>
-                <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3 py-5">
+                <div style={{ position: "relative" }} >
+                    <img src={backgroundImg} alt="" className="img-fluid m-0 p-0" />
+                    <div style={{ position: "absolute", top: " 50%", left: "50%", transform: " translate(-50%, -50%)", color: "black", fontSize: "24px", fontWeight: "bold", textAlign: "center" }} >
+                        <h2 className="text-primary">  <em> Discover some of My works & Experiments</em></h2>
+                    </div>
+                </div>
+                <div className="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3 pt-1">
                     {MY_WORKS.map((project) => {
                         return (<Website key={project.id} project={project} />)
                     })}
