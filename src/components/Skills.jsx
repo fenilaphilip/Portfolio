f6349102b04e103620989fa39d2569348299bd51
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2025-03-14 21:05:22 +0100

    Listing skills with icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,4 @@
 import SKILLS from "../assets/skills.js";
-import softskills from "../media/softskills.jpg";
 import techskills from "../media/techskills.jpg"
 
 export default function Skills() {
@@ -7,14 +6,14 @@ export default function Skills() {
         <section id="skills">
             <div className="container">
                 <div className="row align-items-center">
-                    <div className="col-lg-5">
-                        <img src={techskills} className="img-fluid" alt="soft skills" id="softskillspic" />
+                    <div className="col-lg-4">
+                        <img src={techskills} className="img-fluid" alt="soft skills" id="techskillpic" />
                     </div>
-                    <div className="col-lg-7">
+                    <div className="col-lg-8">
                         <h1 className="text-center text-primary heading"><span>Tools & Technology</span></h1>
                         <div className="row">
                             <div>
-                                {SKILLS.map((skill) => <img className="m-1 p-1" src={skill.symbol} width="80" height="80" />)}
+                                {SKILLS.map((skill) => <img className="m-1 p-1" src={skill.symbol} alt={skill.name} width="80" height="80" />)}
                             </div>
                         </div>
                     </div>
